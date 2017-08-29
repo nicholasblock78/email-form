af1ad4cf4e5de189fdf3b46ca0b169d551e6b8e1
Nick Block <nicholasblock78@gmail.com>
2017-08-29 09:39:58 -0500

    Slightly refactored for MadCap integration

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,9 +1,9 @@
-$(function () {
-  $('#email-btn').on('click', function(e) {
-    // e.preventDefault();
-    $('#email-form').toggle();
-    $('div').show();
-    var href = window.location.href
-    $('#email-description').after("<input type='hidden' name='href' value='"+href+"'>");
-  });
-})
+// $(function () {
+//   $('#email-btn').on('click', function(e) {
+//     // e.preventDefault();
+//     $('#email-form').toggle();
+//     $('div').show();
+//     var href = window.location.href
+//     $('#email-description').after("<input type='hidden' name='href' value='"+href+"'>");
+//   });
+// })
